defb0bb4c057e8f2e329f9a436044d07e1b796ee
mumarqayyum051 <mumarqayyum051>
2022-05-13 10:52:19 +0500

    issues fixed in notification

--- controllers/notifications/notificationsController.js
+++ controllers/notifications/notificationsController.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
 const router = express.Router();
-
+var multer = require("../../utilities/multer");
+var cpUpload = multer.array("file", 1);
 const {
   createNotification,
   searchNotifications,
 } = require("./notificationsService");
 
-router.post("/createNotification", createNotification);
+router.post("/createNotification", cpUpload, createNotification);
 
 router.post("/searchNotifications", searchNotifications);
 module.exports = router;
--- controllers/notifications/notificationsService.js
+++ controllers/notifications/notificationsService.js
@@ -2,16 +2,30 @@ const { BadRequestResponse, OkResponse } = require("express-http-response");
 const db = require("../../db");
 
 const createNotification = (req, res, next) => {
-  const { notificationTypeId, sroNO, year, dated, law_or_statute_id } =
+  const filePath = req.files[0].path;
+  const { notificationTypeId, sroNO, subject, year, dated, law_or_statute_id } =
     req.body || req.body.notification;
+
+  var domain = req.headers.host;
+  var pathname = new URL(filePath).pathname;
+  var serverLink = pathname.split("\\").splice(-2).join("/");
+  const file = domain + "/" + serverLink;
   console.log(req.body);
-  if (!notificationTypeId || !sroNO || !year || !dated || !law_or_statute_id) {
+  if (
+    !notificationTypeId ||
+    !sroNO ||
+    !subject ||
+    !year ||
+    !dated ||
+    !law_or_statute_id ||
+    !file
+  ) {
     return res
       .status(403)
       .send(new BadRequestResponse("Please fill all the fields"));
   }
 
-  const query = `INSERT INTO notifications (notificationTypeId,sroNO,year,dated,law_or_statute_id) VALUES ('${notificationTypeId}', '${sroNO}', '${year}',  '${dated}', '${law_or_statute_id}')`;
+  const query = `INSERT INTO notifications (notificationTypeId,sroNO,subject,year,dated,law_or_statute_id,file) VALUES ('${notificationTypeId}', '${sroNO}', '${subject}','${year}',  '${dated}', '${law_or_statute_id}','${file}')`;
   db.query(query, (err, result) => {
     if (err) {
       return res.send(new BadRequestResponse(err));
@@ -21,33 +35,36 @@ const createNotification = (req, res, next) => {
 };
 
 const searchNotifications = (req, res) => {
-  const { sroNO, year, notificationTypeId, dated, law_or_statute_id } =
+  const { sroNO, year, notificationTypeId, subject, dated, law_or_statute_id } =
     req.body || req.body.notification;
 
   console.log(req.body);
   let search = `SELECT * FROM notifications WHERE `;
   if (sroNO) {
-    search += `sroNO = '${sroNO}' AND `;
+    search += `sroNO LIKE '%${sroNO}%' OR `;
   }
   if (year) {
-    search += `year = '${year}' AND `;
+    search += `year LIKE '%${year}%' OR `;
   }
   if (notificationTypeId) {
-    search += `notificationTypeId = '${notificationTypeId}' AND `;
+    search += `notificationTypeId LIKE '%${notificationTypeId}%' OR `;
+  }
+  if (subject) {
+    search += `subject LIKE '%${subject}%' OR `;
   }
   if (dated) {
-    search += `dated = '${dated}' AND `;
+    search += `dated LIKE '%${dated}%' OR `;
   }
   if (law_or_statute_id) {
-    search += `law_or_statute_id = '${law_or_statute_id}' `;
+    search += `law_or_statute_id LIKE '%${law_or_statute_id}%' `;
   }
-  search = search.split("AND").slice(0, -1).join("AND");
+  search = search.split("OR").slice(0, -1).join("OR");
   db.query(search, (err, result) => {
     console.log(result);
     if (err) {
       return res.send(new BadRequestResponse(err));
     }
-    return res.send(new OkResponse(result[0]));
+    return res.send(new OkResponse(result));
   });
 };
 
